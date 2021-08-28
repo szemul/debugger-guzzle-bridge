f4e6aa652ff9443646e905acda7faa2ec0846cab
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2021-08-29 01:30:39 +1200

    Fix phpstan and php cs fixer

--- tests/Middleware/GuzzleDebuggerMiddlewareTest.php
+++ tests/Middleware/GuzzleDebuggerMiddlewareTest.php
@@ -3,12 +3,11 @@ declare(strict_types=1);
 
 namespace Szemul\DebuggerGuzzleBridge\Test\Middleware;
 
-use Szemul\DebuggerGuzzleBridge\Middleware\GuzzleDebuggerMiddleware;
 use PHPUnit\Framework\TestCase;
 
 class GuzzleDebuggerMiddlewareTest extends TestCase
 {
-    public function testDummy()
+    public function testDummy(): void
     {
         // Dummy test to actually trigger the code coverage so codecov works
         $this->assertTrue(true);
