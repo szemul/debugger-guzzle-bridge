d581dc3624c75d6f3364278da459eed22ca086f4
Zsolt Szeberenyi <zsolt@szeberenyi.com>
2021-08-29 01:26:45 +1200

    Add empty test

---
+++ tests/Middleware/GuzzleDebuggerMiddlewareTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Szemul\DebuggerGuzzleBridge\Test\Middleware;
+
+use Szemul\DebuggerGuzzleBridge\Middleware\GuzzleDebuggerMiddleware;
+use PHPUnit\Framework\TestCase;
+
+class GuzzleDebuggerMiddlewareTest extends TestCase
+{
+    public function testDummy()
+    {
+        // Dummy test to actually trigger the code coverage so codecov works
+        $this->assertTrue(true);
+    }
+
+    // TODO implement
+}
